f863a8464b279c764c157a11c29e2f66f56d8542
Foxxlarr <talismafeed@yandex.ru>
2022-11-14 18:24:00 +0300

    method created

--- Final1Q/Program.cs
+++ Final1Q/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Input something to find out what is shorter than four symbols(use space to separate)");
+string line = System.Console.ReadLine();
+
+string symbolCount(string line)
+{
+string[] M = line.Split(' ');
+var result = new string[M.Length];
+var realSize = 0;
+foreach (var value in M)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+string row = (string.Join(" ", result, 0, realSize));
+return row;
+}
+
